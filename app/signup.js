52f45154739138f832d88c672b4df2a31d83c7c6
MohmmadNada <mohamma97dnada@gmail.com>
2023-02-01 11:12:56 +0300

    Finish Realtime Validation as OOP

--- app/signup.js
+++ app/signup.js
@@ -85,13 +85,136 @@ class InputComponent {
     from.appendChild(InputContainer);
   }
 }
-
 class EmailComponent extends InputComponent {
   constructor() {
     super(document.querySelector("#container"), "email");
+    document
+      .getElementById("email")
+      .addEventListener("input", EmailComponent.isValidEmail);
   }
 
+  static isValidEmail() {
+    const email = document.getElementById("email");
+    const validationIcon = document.querySelector(
+      `#${email.id} + .validation-check`
+    );
+    validationIcon.style.display = "inline";
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+      validationIcon.innerHTML = "&#10003;";
+      return email.value;
+    } else {
+      validationIcon.innerHTML = "&#x2718;";
+      return false;
+    }
+  }
 }
+
+class UsernameComponent extends InputComponent {
+  constructor() {
+    super(document.querySelector("#container"), "username");
+    document
+      .getElementById("username")
+      .addEventListener("input", UsernameComponent.isValidUsername);
+  }
+
+  static isValidUsername() {
+    const username = document.getElementById("username");
+    const validationIcon = document.querySelector(
+      `#${username.id} + .validation-check`
+    );
+    validationIcon.style.display = "inline";
+    if (/^[a-z][a-z\-\ ]+$/.test(username.value)) {
+      validationIcon.innerHTML = "&#10003;";
+      return username.value;
+    } else {
+      validationIcon.innerHTML = "&#x2718;";
+      return false;
+    }
+  }
+}
+
+class Password1Component extends InputComponent {
+  constructor() {
+    super(document.querySelector("#container"), "password-1");
+    document
+      .getElementById("password-1")
+      .addEventListener("input", Password1Component.isValidPassword);
+  }
+
+  static isValidPassword() {
+    const firstPassword = document.getElementById("password-1");
+    const validationIcon = document.querySelector(
+      `#${firstPassword.id} + .validation-check`
+    );
+    validationIcon.style.display = "inline";
+    let isMeetPasswordReqs = [];
+    // Check if it contain at least one Capital letter
+    if (
+      /[A-Z]/.test(firstPassword.value) &&
+      /[a-z]/.test(firstPassword.value)
+    ) {
+      isMeetPasswordReqs.push(true);
+    } else {
+      isMeetPasswordReqs.push(false);
+    }
+    // Check if it contain at least one Special Char
+    if (/[_&@*#$!\-]/g.test(firstPassword.value)) {
+      isMeetPasswordReqs.push(true);
+    } else {
+      isMeetPasswordReqs.push(false);
+    }
+    // Check if it contain at least one Number
+    if (/[\d]/.test(firstPassword.value)) {
+      isMeetPasswordReqs.push(true);
+    } else {
+      isMeetPasswordReqs.push(false);
+    }
+    if (firstPassword.value.length >= 8) {
+      // The length must be more than 7 chars
+      isMeetPasswordReqs.push(true);
+    } else {
+      isMeetPasswordReqs.push(false);
+    }
+    if (isMeetPasswordReqs.every((isMeet) => isMeet == true)) {
+      validationIcon.innerHTML = "&#10003;";
+      return firstPassword.value;
+    } else {
+      validationIcon.innerHTML = "&#x2718;";
+      return false;
+    }
+  }
+}
+
+class ConfirmPasswordComponent extends InputComponent {
+  constructor() {
+    super(document.querySelector("#container"), "password-2");
+    document
+      .getElementById("password-2")
+      .addEventListener("input", ConfirmPasswordComponent.isPasswordMatch);
+  }
+
+  static isPasswordMatch() {
+    const firstPassword = document.getElementById("password-1");
+    // On submit
+    const secondPassword = document.getElementById("password-2");
+    const validationIcon = document.querySelector(
+      `#${secondPassword.id} + .validation-check`
+    );
+    validationIcon.style.display = "inline";
+    if (!firstPassword.value.length) {
+      validationIcon.innerHTML = "&#x2718;";
+      return false;
+    }
+    if (secondPassword.value == firstPassword.value) {
+      validationIcon.innerHTML = "&#10003;";
+      return true;
+    } else {
+      validationIcon.innerHTML = "&#x2718;";
+      return false;
+    }
+  }
+}
+
 class GenderRadioComponent {
   constructor(form, genderType) {
     const ButtonsContainer = document.createElement("div");
@@ -116,11 +239,10 @@ class SignUpForm {
   constructor() {
     const formElement = SignUpForm.createSignUpForm();
     SignUpForm.createRegistrationHeader(formElement);
-    // new InputComponent(formElement, "email");
     new EmailComponent();
-    new InputComponent(formElement, "username");
-    new InputComponent(formElement, "password-1");
-    new InputComponent(formElement, "password-2");
+    new UsernameComponent();
+    new Password1Component();
+    new ConfirmPasswordComponent();
     new CountryComponent(formElement);
     new GenderRadioComponent(formElement, "male");
     new GenderRadioComponent(formElement, "female");
@@ -185,127 +307,11 @@ class SignUpPage {
 
 new SignUpPage();
 
-const emailInput = document.getElementById("email");
-emailInput.addEventListener("input", isValidEmail);
-const usernameInput = document.getElementById("username");
-usernameInput.addEventListener("input", isValidUsername);
-const firstPasswordInput = document.getElementById("password-1");
-firstPasswordInput.addEventListener("input", isValidPassword);
-const secondPasswordInput = document.getElementById("password-2");
-secondPasswordInput.addEventListener("input", isPasswordMatch);
-// The country should be selected
 const formElement = document.getElementsByTagName("form");
-
-function isValidEmail() {
-  const email = document.getElementById("email");
-  const validationIcon = document.querySelector(
-    `#${email.id} + .validation-check`
-  );
-  validationIcon.style.display = "inline";
-  if (!email.value.length) {
-    validationIcon.innerHTML = "&#x2718;";
-    createErrorMsg("empty field");
-    return true;
-  }
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
-    validationIcon.innerHTML = "&#10003;";
-    return email.value;
-  } else {
-    validationIcon.innerHTML = "&#x2718;";
-    return false;
-  }
-}
-
-function isValidUsername() {
-  const username = document.getElementById("username");
-  const validationIcon = document.querySelector(
-    `#${username.id} + .validation-check`
-  );
-  validationIcon.style.display = "inline";
-  if (!username.value.length) {
-    validationIcon.innerHTML = "&#x2718;";
-    createErrorMsg("empty field");
-    return true;
-  }
-  if (/^[a-z][a-z\-\ ]+$/.test(username.value)) {
-    validationIcon.innerHTML = "&#10003;";
-    return username.value;
-  } else {
-    validationIcon.innerHTML = "&#x2718;";
-    return false;
-  }
-}
-
-function isValidPassword() {
-  const firstPassword = document.getElementById("password-1");
-  const validationIcon = document.querySelector(
-    `#${firstPassword.id} + .validation-check`
-  );
-  validationIcon.style.display = "inline";
-  let isMeetPasswordReqs = [];
-  // Check if it contain at least one Capital letter
-  if (!firstPassword.value.length) {
-    validationIcon.innerHTML = "&#x2718;";
-    createErrorMsg("empty field");
-    return true;
-  }
-  if (/[A-Z]/.test(firstPassword.value) && /[a-z]/.test(firstPassword.value)) {
-    isMeetPasswordReqs.push(true);
-  } else {
-    isMeetPasswordReqs.push(false);
-  }
-  // Check if it contain at least one Special Char
-  if (/[_&@*#$!\-]/g.test(firstPassword.value)) {
-    isMeetPasswordReqs.push(true);
-  } else {
-    isMeetPasswordReqs.push(false);
-  }
-  // Check if it contain at least one Number
-  if (/[\d]/.test(firstPassword.value)) {
-    isMeetPasswordReqs.push(true);
-  } else {
-    isMeetPasswordReqs.push(false);
-  }
-  if (firstPassword.value.length >= 8) {
-    // The length must be more than 7 chars
-    isMeetPasswordReqs.push(true);
-  } else {
-    isMeetPasswordReqs.push(false);
-  }
-  if (isMeetPasswordReqs.every((isMeet) => isMeet == true)) {
-    validationIcon.innerHTML = "&#10003;";
-    return firstPassword.value;
-  } else {
-    validationIcon.innerHTML = "&#x2718;";
-    return false;
-  }
-}
-
-function isPasswordMatch() {
-  const firstPassword = document.getElementById("password-1");
-  // On submit
-  const secondPassword = document.getElementById("password-2");
-  const validationIcon = document.querySelector(
-    `#${secondPassword.id} + .validation-check`
-  );
-  validationIcon.style.display = "inline";
-  if (!firstPassword.value.length) {
-    validationIcon.innerHTML = "&#x2718;";
-    createErrorMsg("empty field");
-    return true;
-  }
-  if (secondPassword.value == firstPassword.value) {
-    validationIcon.innerHTML = "&#10003;";
-    return true;
-  } else {
-    validationIcon.innerHTML = "&#x2718;";
-    return false;
-  }
-}
-
 
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
+  // Prevent Default =>
   let meetReq = new Set();
   let emailValue;
   let usernameValue;
